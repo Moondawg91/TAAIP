4b6c864a73e9dd14bad1c933d65401c52d530c78
Amber Mooney <ambermooney@Mac.local.tld>
2025-11-24 12:15:51 -0500

    Frontend: add ErrorBoundary and wrap root to avoid blank screens on errors

--- taaip-dashboard/src/main.tsx
+++ taaip-dashboard/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import ErrorBoundary from './components/ErrorBoundary'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary fallbackTitle="We hit a snag" fallbackMessage="A dashboard failed to render. Try reloading.">
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
 )
